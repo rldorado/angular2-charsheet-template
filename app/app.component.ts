01e49d80e2a96aa7f8e466b260759bb654169635
rafael <rafael.lopez@fon.com>
2016-05-31 18:47:43 +0200

    Added first hero test sheet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,34 @@
 import { Component } from '@angular/core';
+
+export class Hero {
+    id: number;
+    name: string;
+    race: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template:`
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} character sheet</h2>
+        <div><label>ID: </label>{{hero.id}}</div>
+        <div>
+            <label>Name: </label>
+            <input [(ngModel)]="hero.name" placeholder="Write the name..">
+        </div>
+        <div>
+            <label>Race: </label>
+            <input [(ngModel)]="hero.race" placeholder="Write the race..">
+        </div>
+        `
+
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'NWNDragonlance characters';
+    hero: Hero = {
+        id: 1,
+        name: 'Louie Loire',
+        race: 'Human'
+    };
+}
